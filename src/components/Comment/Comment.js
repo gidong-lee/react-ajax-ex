8503325385955b36000ee7afd2202da7d9902307
gidong.lee <gidong.lee@sk.com>
2017-02-13 18:47:03 +0900

    2nd commit

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Comment.css';
+
+const Comment = () => {
+  return (
+    <li className="Comment">
+      <p>
+        <b>name</b> Body
+      </p>
+    </li>
+  );
+};
+
+export default Comment;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Header from './Header/Header.js';
+import PostWrapper from './PostWrapper/PostWrapper.js';
+import Navigate from './Navigate/Navigate.js';
+import Post from './Post/Post.js';
+import CommentList from './CommentList/CommentList.js';
+import Comment from './Comment/Comment.js';
+
+export {
+    Header,
+    PostWrapper,
+    Navigate,
+    Post,
+    CommentList,
+    Comment
+};
