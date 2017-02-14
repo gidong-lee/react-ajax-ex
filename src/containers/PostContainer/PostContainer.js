f87a97e018d8c7ef80be590106360bd2b23939e6
gidong.lee <gidong.lee@sk.com>
2017-02-14 11:30:55 +0900

    ajax

--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import { PostWrapper, Navigate, Post} from '../../components';
+import * as service from '../../service/post.js';
 
 class PostContainer extends Component {
+
+  componentDidMount() {
+    this.fetchPostInfo(1);
+  };
+
+  fetchPostInfo = async (postId) => {
+    const post = await service.getPost(postId);
+    console.log(post);
+    const comments = await service.getComments(postId);
+    console.log(comments);
+  };
+
   render() {
     return (
       <PostWrapper>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,4 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render( <App />, document.getElementById('root'));
